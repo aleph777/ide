1c14e50f81c7673f151f3c3a0c0b0cfa88541cc2
Tom Fontaine <thomas.fontaine@altran.com>
2020-10-05 11:46:42 -0400

    changed defvar to defconst

--- elisp/themes/colors/color-blind.el
+++ elisp/themes/colors/color-blind.el
@@ -1,9 +1,9 @@
-;;; colors-color-blind.el --- color definitions -*-Emacs-Lisp-*-
+;;; color-blind.el --- color definitions -*-Emacs-Lisp-*-
 
 ;;         Copyright © 2020-2020  Tom Fontaine
 
-(defvar color-blind/gold-20        "#ffb000")
-(defvar color-blind/indigo-50      "#785ef0")
-(defvar color-blind/magenta-50     "#dc267f")
-(defvar color-blind/orange-40      "#fe6100")
-(defvar color-blind/ultramarine-40 "#648fff")
+(defconst color-blind/gold-20        "#ffb000")
+(defconst color-blind/indigo-50      "#785ef0")
+(defconst color-blind/magenta-50     "#dc267f")
+(defconst color-blind/orange-40      "#fe6100")
+(defconst color-blind/ultramarine-40 "#648fff")
