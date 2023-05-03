f680b2af4da009a25300f95b0b91b41904566011
aleph777 <31519141+aleph777@users.noreply.github.com>
2023-05-03 10:39:55 -0400

    Fixed undo-tree-history-directory-alist setting

--- elisp/custom.el
+++ elisp/custom.el
@@ -18,8 +18,6 @@
  '(custom-enabled-themes '(fontaine))
  '(custom-safe-themes t)
  '(flycheck-flake8-maximum-line-length 200)
- '(package-selected-packages
-   '(dash consult consult-flycheck consult-selectrum marginalia selectrum ivy-posframe wgrep smex flx counsel ivy-rich ivy-hydra ivy unicode-fonts emojify powerline vterm anti-zenburn-theme acme-theme fontaine-theme rainbow-mode ws-butler treemacs treemacs-magit treemacs-projectile minions filladapt u-cc ide-cpp clean-aindent-mode groovy ccls company-rtags flycheck-rtags rtags ggtags makefile-executor groovy-mode company-lsp lsp-ui eglot declutter org-mode tinyeat tetrist pretty-column elisp--witness--lisp magit async anzu bm clang-format clips-mode company-jedi company-plsense company-ycmd csharp-mode cuda-mode delight ergoemacs-functions ergoemacs-mode f flycheck-pos-tip flycheck-ycmd highlight-escape-sequences highlight-operators json-mode langtool loccur lua-mode matlab-mode mic-paren modern-cpp-font-lock neotree nxml-mode org-cua-dwim paradox perl6-mode plsense popwin rainbow-delimiters shift-number smartparens smooth-scrolling ssh tabbar undo-tree use-package volatile-highlights web-mode yaml-mode ycmd))
  '(paradox-github-token t)
  '(powerline-gui-use-vcs-glyph t)
  '(rainbow-x-colors nil)
@@ -33,7 +31,7 @@
               1 'font-lock-variable-name-face)))
      (enable-local-variables: . all)))
  '(tab-width 4)
- '(undo-tree-history-directory-alist '(("~undo-tree~" . "~/.config/emacs/undo-tree"))))
+ '(undo-tree-history-directory-alist '(("." . "~/.config/emacs/undo-tree/"))))
 
 (message "setting faces...")
 
@@ -45,3 +43,4 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  )
+
