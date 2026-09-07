e98be788012032603ce79c9e4ad74b64ae0fdb7f
aleph777 <31519141+aleph777@users.noreply.github.com>
2026-09-07 17:15:01 -0400

    Create honeywell.el

---
+++ elisp/themes/colors/honeywell.el
@@ -0,0 +1,16 @@
+;;; honeywell.el --- color definitions -*-lexical-binding: t-*- ;; -*-Emacs-Lisp-*-
+
+;;         Copyright © 2020-2026  Tom Fontaine
+
+(defconst honeywell/red         "#de0031") ;; medium red-magenta, text: white
+(defconst honeywell/total-black "#191a00") ;; black yellow, text: white
+(defconst honeywell/white       "#ffffff") ;; white gray (red), text: black
+(defconst honeywell/dark-gray   "#666666") ;; medium gray (red), text: white
+(defconst honeywell/light-gray  "#d6d7d6") ;; light gray (green), text: black
+(defconst honeywell/deep-blue   "#2e6099") ;; medium blue-cyan, text: white
+(defconst honeywell/dark-green  "#3d9240") ;; medium green, text: white
+(defconst honeywell/light-green "#8ba862") ;; medium yellow-green, text: black
+(defconst honeywell/bright-blue "#3b87de") ;; medium blue-cyan, text: white
+(defconst honeywell/tan         "#d0a660") ;; medium yellow-red, text: black
+
+(defconst honeywell/red-1       "#f80036") ;; medium red-magenta, text: white
