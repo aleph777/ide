127f3dff94f1e6427ece359c9ccebc8691dfb9a6
aleph777 <31519141+aleph777@users.noreply.github.com>
2026-09-07 17:10:52 -0400

    Update .emacs.el

--- .emacs.el
+++ .emacs.el
@@ -9,20 +9,20 @@
 ;;
 (require 'cl-lib)
 
-(defvar is-daemon?    (daemonp))
-(defvar is-gui?       (display-graphic-p))
+(defconst is-daemon?    (daemonp))
+(defconst is-gui?       (display-graphic-p))
 
-(defvar is-cygwin?    (eq system-type 'cygwin))
-(defvar is-linux?     (eq system-type 'gnu/linux))
-(defvar is-macos?     (eq system-type 'darwin))
-(defvar is-windows?   (eq system-type 'windows-nt))
-(defvar is-wsl?       (not (equal user-login-name "fontaine")))
+(defconst is-cygwin?    (eq system-type 'cygwin))
+(defconst is-linux?     (eq system-type 'gnu/linux))
+(defconst is-macos?     (eq system-type 'darwin))
+(defconst is-windows?   (eq system-type 'windows-nt))
+(defconst is-wsl?       (equal (system-name) "HON-366GKK4"))
+(defconst is-linux-gui? (and is-linux? is-gui?))
 
-(defvar is-linux-gui? (and is-linux? is-gui?))
-
-(defvar tjf:user/windows-id       "tfontaine")
+(defvar tjf:user/windows-id       "H679869")
 (defvar tjf:user/copyright-holder user-full-name)
 (defvar tjf:user/dir-home         (concat (getenv "HOME") "/"))
+(defvar tjf:user/dir-emacs.d      user-emacs-directory)
 
 ;;
 (defvar tjf:user/dir-bin      (concat tjf:user/dir-home "bin/"))
@@ -35,17 +35,23 @@
 
 (defvar tjf:user/dir-themes   (concat tjf:user/dir-elisp "themes/"))
 
-(defvar tjf:user/dir-elisp-ext    (concat tjf:user/dir-elisp "ext/"))
-(defvar tjf:user/dir-elisp-images (concat tjf:user/dir-elisp "images/"))
-(defvar tjf:user/dir-elisp-tjf    (concat tjf:user/dir-elisp "tjf/"))
+(defvar tjf:user/dir-elisp-ext     (concat tjf:user/dir-elisp "ext/"))
+(defvar tjf:user/dir-elisp-images  (concat tjf:user/dir-elisp "images/"))
+(defvar tjf:user/dir-elisp-toolbar (concat tjf:user/dir-elisp "images/toolbar/"))
+(defvar tjf:user/dir-elisp-tjf     (concat tjf:user/dir-elisp "tjf/"))
 
-(defvar tjf:user/dir-initialize (concat tjf:user/dir-elisp "00_initialize/"))
-(defvar tjf:user/dir-experiment (concat tjf:user/dir-elisp "00_experiment/"))
+(if is-linux?
+    (let ((cmd-distro  "lsb_release -is | tr [:upper:] [:lower:] | tr -d '\n'")
+          (cmd-version "lsb_release -rs | cut -d. -f1 | tr -d '\n'"))
+      (setq tjf:user/os-distro  (shell-command-to-string cmd-distro))
+      (setq tjf:user/os-version (shell-command-to-string cmd-version))
+      (setq tjf:user/os-distro-version (concat tjf:user/os-distro " " tjf:user/os-version))))
 
 ;;
-(cl-pushnew tjf:user/dir-elisp-images image-load-path :test 'string=)
-(cl-pushnew tjf:user/dir-elisp-ext  load-path :test 'string=)
-(cl-pushnew tjf:user/dir-elisp-tjf  load-path :test 'string=)
+(cl-pushnew tjf:user/dir-elisp-images  image-load-path :test 'string=)
+(cl-pushnew tjf:user/dir-elisp-toolbar image-load-path :test 'string=)
+(cl-pushnew tjf:user/dir-elisp-ext     load-path       :test 'string=)
+(cl-pushnew tjf:user/dir-elisp-tjf     load-path       :test 'string=)
 
 (setq user-emacs-directory tjf:user/dir-config)
 (setq custom-file (concat tjf:user/dir-elisp "custom.el"))
@@ -55,5 +61,6 @@
 
 ;;
 (message "Loading .emacs.el...done")
+(provide 'init)
 
 ;;; .emacs.el ends here
